31201b4334db9d4324e58a17fc527e5218bac44c
Negoiu <negoiupaulica21@gmail.com>
2022-10-06 03:09:21 +0300

    stuff

--- src/main/java/com/fasttrackit/JavaEncyclopediaProject/article/ArticleRepository.java
+++ src/main/java/com/fasttrackit/JavaEncyclopediaProject/article/ArticleRepository.java
@@ -2,8 +2,6 @@ package com.fasttrackit.JavaEncyclopediaProject.article;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
--- src/main/java/com/fasttrackit/JavaEncyclopediaProject/article/ArticleService.java
+++ src/main/java/com/fasttrackit/JavaEncyclopediaProject/article/ArticleService.java
@@ -2,7 +2,6 @@ package com.fasttrackit.JavaEncyclopediaProject.article;
 
 import com.fasttrackit.JavaEncyclopediaProject.exceptions.NullFieldException;
 import com.fasttrackit.JavaEncyclopediaProject.exceptions.ResourceNotFoundException;
-import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
